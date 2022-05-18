fe593a85249d62e9dbea2c5f026a0fd9f5a540fd
juan_clarembaux <juanclarembaux12@gmail.com>
2022-05-18 20:54:27 -0300

    Le agregue funcionalidad a todos los objetos de cada activity

--- app/src/main/java/com/example/segundaapp/SecondActivity.java
+++ app/src/main/java/com/example/segundaapp/SecondActivity.java
@@ -2,13 +2,110 @@ package com.example.segundaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private ImageView activitySecond_gatitoIMG;
+    private EditText activitySecond_numparaop_et;
+    private Button activitySecond_sumar_bt, activitySecond_restar_bt;
+    private Button activitySecond_zoomPositivo_bt, activitySecond_zoomNegativo_bt;
+    private Button activitySecond_resetear_bt, activitySecond_ocultar_bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        activitySecond_gatitoIMG = findViewById(R.id.activitySecond_gatitoIMG);
+        activitySecond_numparaop_et = findViewById(R.id.activitySecond_numparaop_et);
+        activitySecond_sumar_bt = findViewById(R.id.activitySecond_sumar_bt);
+        activitySecond_restar_bt = findViewById(R.id.activitySecond_restar_bt);
+        activitySecond_zoomPositivo_bt = findViewById(R.id.activitySecond_zoomPositivo_bt);
+        activitySecond_zoomNegativo_bt = findViewById(R.id.activitySecond_zoomNegativo_bt);
+        activitySecond_resetear_bt = findViewById(R.id.activitySecond_resetear_bt);
+        activitySecond_ocultar_bt = findViewById(R.id.activitySecond_ocultar_bt);
+        this.findViews();
     }
+
+    private void findViews(){
+        activitySecond_sumar_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activitySecond_numparaop_et.setText(String.valueOf(sumar(Integer.parseInt(activitySecond_numparaop_et.getText().toString())) + ""));
+            }
+        });
+        activitySecond_restar_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activitySecond_numparaop_et.setText(String.valueOf(restar(Integer.parseInt(activitySecond_numparaop_et.getText().toString())) + ""));
+            }
+        });
+        activitySecond_resetear_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activitySecond_numparaop_et.setText("0");
+            }
+        });
+        activitySecond_ocultar_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activitySecond_gatitoIMG.setVisibility(View.INVISIBLE);
+                activitySecond_numparaop_et.setVisibility(View.INVISIBLE);
+            }
+        });
+        activitySecond_zoomNegativo_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activitySecond_gatitoIMG.setScaleX((activitySecond_gatitoIMG.getScaleX() - 0.1f));
+                activitySecond_gatitoIMG.setScaleY((activitySecond_gatitoIMG.getScaleY() - 0.1f));
+                activitySecond_numparaop_et.setScaleX((activitySecond_numparaop_et.getScaleX() - 0.1f));
+                activitySecond_numparaop_et.setScaleY((activitySecond_numparaop_et.getScaleY() - 0.1f));
+                activitySecond_sumar_bt.setScaleX((activitySecond_sumar_bt.getScaleX() - 0.1f));
+                activitySecond_sumar_bt.setScaleY((activitySecond_sumar_bt.getScaleY() - 0.1f));
+                activitySecond_restar_bt.setScaleX((activitySecond_restar_bt.getScaleX() - 0.1f));
+                activitySecond_restar_bt.setScaleY((activitySecond_restar_bt.getScaleY() - 0.1f));
+                activitySecond_zoomPositivo_bt.setScaleX((activitySecond_zoomPositivo_bt.getScaleX() - 0.1f));
+                activitySecond_zoomPositivo_bt.setScaleY((activitySecond_zoomPositivo_bt.getScaleY() - 0.1f));
+                activitySecond_zoomNegativo_bt.setScaleX((activitySecond_zoomNegativo_bt.getScaleX() - 0.1f));
+                activitySecond_zoomNegativo_bt.setScaleY((activitySecond_zoomNegativo_bt.getScaleY() - 0.1f));
+                activitySecond_resetear_bt.setScaleX((activitySecond_resetear_bt.getScaleX() - 0.1f));
+                activitySecond_resetear_bt.setScaleY((activitySecond_resetear_bt.getScaleY() - 0.1f));
+                activitySecond_ocultar_bt.setScaleX((activitySecond_ocultar_bt.getScaleX() - 0.1f));
+                activitySecond_ocultar_bt.setScaleY((activitySecond_ocultar_bt.getScaleY() - 0.1f));
+            }
+        });
+        activitySecond_zoomPositivo_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activitySecond_gatitoIMG.setScaleX((activitySecond_gatitoIMG.getScaleX() + 0.1f));
+                activitySecond_gatitoIMG.setScaleY((activitySecond_gatitoIMG.getScaleY() + 0.1f));
+                activitySecond_numparaop_et.setScaleX((activitySecond_numparaop_et.getScaleX() + 0.1f));
+                activitySecond_numparaop_et.setScaleY((activitySecond_numparaop_et.getScaleY() + 0.1f));
+                activitySecond_sumar_bt.setScaleX((activitySecond_sumar_bt.getScaleX() + 0.1f));
+                activitySecond_sumar_bt.setScaleY((activitySecond_sumar_bt.getScaleY() + 0.1f));
+                activitySecond_restar_bt.setScaleX((activitySecond_restar_bt.getScaleX() + 0.1f));
+                activitySecond_restar_bt.setScaleY((activitySecond_restar_bt.getScaleY() + 0.1f));
+                activitySecond_zoomPositivo_bt.setScaleX((activitySecond_zoomPositivo_bt.getScaleX() + 0.1f));
+                activitySecond_zoomPositivo_bt.setScaleY((activitySecond_zoomPositivo_bt.getScaleY() + 0.1f));
+                activitySecond_zoomNegativo_bt.setScaleX((activitySecond_zoomNegativo_bt.getScaleX() + 0.1f));
+                activitySecond_zoomNegativo_bt.setScaleY((activitySecond_zoomNegativo_bt.getScaleY() + 0.1f));
+                activitySecond_resetear_bt.setScaleX((activitySecond_resetear_bt.getScaleX() + 0.1f));
+                activitySecond_resetear_bt.setScaleY((activitySecond_resetear_bt.getScaleY() + 0.1f));
+                activitySecond_ocultar_bt.setScaleX((activitySecond_ocultar_bt.getScaleX() + 0.1f));
+                activitySecond_ocultar_bt.setScaleY((activitySecond_ocultar_bt.getScaleY() + 0.1f));
+            }
+        });
+    }
+    public int sumar (int a){
+        return a + 1;
+    }
+    public int restar (int a){ return a - 1; }
+
 }
--- app/src/main/java/com/example/segundaapp/ThirdActivity.java
+++ app/src/main/java/com/example/segundaapp/ThirdActivity.java
@@ -2,13 +2,66 @@ package com.example.segundaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class ThirdActivity extends AppCompatActivity {
+
+    private EditText activityThird_ingreseMonto_et;
+    private RadioButton activityThird_dolar_rbt, activityThird_real_rbt, activityThird_euro_rbt;
+    private TextView activityThird_resultadoMonto_tv;
+    private Button activityThird_convertir_bt, activityThird_reiniciar_bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        activityThird_ingreseMonto_et = findViewById(R.id.activityThird_ingreseMonto_et);
+        activityThird_dolar_rbt = findViewById(R.id.activityThird_dolar_rbt);
+        activityThird_real_rbt = findViewById(R.id.activityThird_real_rbt);
+        activityThird_euro_rbt = findViewById(R.id.activityThird_euro_rbt);
+        activityThird_resultadoMonto_tv = findViewById(R.id.activityThird_resultadoMonto_tv);
+        activityThird_convertir_bt = findViewById(R.id.activityThird_convertir_bt);
+        activityThird_reiniciar_bt = findViewById(R.id.activityThird_reiniciar_bt);
+        this.findViews();
+    }
+
+    private void findViews(){
+        activityThird_convertir_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (activityThird_dolar_rbt.isChecked() == true){
+                    activityThird_resultadoMonto_tv.setText(convertirEnDolar(Double.parseDouble(activityThird_ingreseMonto_et.getText().toString())) + "");
+                }
+                if (activityThird_real_rbt.isChecked() == true){
+                    activityThird_resultadoMonto_tv.setText(convertirEnReal(Double.parseDouble(activityThird_ingreseMonto_et.getText().toString())) + "");
+                }
+                if (activityThird_euro_rbt.isChecked() == true){
+                    activityThird_resultadoMonto_tv.setText(convertirEnEuro(Double.parseDouble(activityThird_ingreseMonto_et.getText().toString())) + "");
+                }
+            }
+        });
+        activityThird_reiniciar_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activityThird_ingreseMonto_et.setText("");
+                activityThird_resultadoMonto_tv.setText("");
+            }
+        });
+    }
+    public double convertirEnDolar(double a){
+        return a * 208;
+    }
+    public double convertirEnReal(double a){
+        return a * 51;
+    }
+    public double convertirEnEuro(double a){
+        return a * 231;
     }
 }
